f812763b3352aed9700e8c7ee9afe8ac5b1f1730
s.locorotondo <steven.locorotondo@ynap.com>
2021-01-02 09:48:32 +0000

    first commit

---
+++ src/spark_utils.py
@@ -0,0 +1,18 @@
+from pyspark.sql import SparkSession
+
+
+def create_spark_session(name):
+    """
+    Creates spark session.
+
+    :param name: string
+    :return: None
+    """
+    spark = SparkSession \
+        .builder \
+        .appName(name) \
+        .config('spark.executor.memory', '30g') \
+        .enableHiveSupport() \
+        .getOrCreate()
+    return spark
+
