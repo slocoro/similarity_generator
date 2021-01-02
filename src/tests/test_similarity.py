9f1ddd3c5c7b513fc9046319bcf65c504b80f354
s.locorotondo <steven.locorotondo@ynap.com>
2021-01-02 21:13:54 +0000

    fix typo

--- src/tests/test_similarity.py
+++ src/tests/test_similarity.py
@@ -22,7 +22,7 @@ class TestSimiarity(PySparkTestCase):
         with self.assertRaises(AssertionError):
             Similarity(df_features=pd_df_simple_table)
 
-    def test_check_nulls_in_attribute_columns(self):
+    def test_check_nulls_in_feature_columns(self):
 
         df_nulls_features = self.spark.read.csv('tests/fixtures/similarity/nulls_features.csv', header=True)
         df_no_nulls_features = self.spark.read.csv('tests/fixtures/similarity/no_nulls_features.csv', header=True)
