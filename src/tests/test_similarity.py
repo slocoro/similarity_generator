fd8a8a32610876bf6eb412ae06dd3d357b7e6f4f
s.locorotondo <steven.locorotondo@ynap.com>
2021-01-02 21:23:37 +0000

    fix fixtures; remove print and show statements

--- src/tests/test_similarity.py
+++ src/tests/test_similarity.py
@@ -14,9 +14,6 @@ class TestSimiarity(PySparkTestCase):
         df_simple_table = self.spark.read.csv('tests/fixtures/similarity/simple_table.csv', header=True)
         pd_df_simple_table = pd.read_csv('tests/fixtures/similarity/simple_table.csv')
 
-        df_simple_table.show()
-        print(pd_df_simple_table)
-
         Similarity(df_features=df_simple_table)
 
         with self.assertRaises(AssertionError):
