7f69844e558f892ec9f328a6a233f41279233969
s.locorotondo <steven.locorotondo@ynap.com>
2021-01-03 12:47:24 +0000

    add code to generate similarity

--- src/main.py
+++ src/main.py
@@ -1,18 +1,50 @@
 from spark_utils import create_spark_session
 
-from preprocess import Preprocessor
+from preprocess import Preprocess
+from similarity import Similarity
+from utils import create_timestamp
+from utils import create_parameters_table
+
+import os
 
 
 spark = create_spark_session('generate_similarities')
 
 df_recipe_info = spark.read.csv('data/recipes_info.csv', header=True)
-columns = 'all'
 
-preprocessor = Preprocessor(df_recipe_info=df_recipe_info,
-                            columns='all')
+COLUMNS = 'all'
+INDEX_COLUMN = 'recipe_id'
+SIMILARITY_TYPE = 'cosine'
 
+etl_created = create_timestamp()
+
+preprocessor = Preprocess(df_recipe_info=df_recipe_info,
+                          columns='all')
 df_recipe_features = preprocessor.preprocess()
+pd_df_recipe_features = df_recipe_features.toPandas()
+features_dir = f'output/{etl_created}/features'
+os.makedirs(features_dir)
+pd_df_recipe_features.to_csv(f'{features_dir}/features.csv', index=False)
 
-df_recipe_features.show()
 
-print(df_recipe_features.dtypes)
+similarity = Similarity(df_features=df_recipe_features,
+                        index_column=INDEX_COLUMN,
+                        similarity_type=SIMILARITY_TYPE)
+similarities = similarity.generate()
+pd_df_similarities_wide = similarities[0]
+pd_df_similarities_long = similarities[1]
+
+similarities_dir = f'output/{etl_created}/similarities'
+os.makedirs(similarities_dir)
+pd_df_similarities_wide.to_csv(f'{similarities_dir}/similarities_wide_index.csv', index=True)
+pd_df_similarities_long.to_csv(f'{similarities_dir}/similarities_long.csv', index=False)
+
+parameters_dir = f'output/{etl_created}/parameters'
+pd_df_parameters = create_parameters_table(similarity_type=SIMILARITY_TYPE,
+                                           index_column=INDEX_COLUMN,
+                                           columns=COLUMNS)
+pd_df_parameters.to_csv(f'{parameters_dir}/parameters.csv', index=False)
+
+
+spark.stop()
+
