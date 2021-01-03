bb5e3b681f7f5721a01653edad8f680d62433dbe
s.locorotondo <steven.locorotondo@ynap.com>
2021-01-03 12:45:33 +0000

    add create_parameters_table

--- src/tests/test_utils.py
+++ src/tests/test_utils.py
@@ -1,6 +1,7 @@
 import unittest
 
 from utils import create_timestamp
+from utils import create_parameters_table
 
 
 class TestUtils(unittest.TestCase):
@@ -12,4 +13,27 @@ class TestUtils(unittest.TestCase):
         self.assertTrue('_' in timestamp)
         self.assertTrue(len(timestamp), 4+2+2+1+4)
 
+    def test_create_parameters_table(self):
+
+        columns_all = 'all'
+        columns_sub = ['country', 'protein']
+        similarity_type = 'cosine'
+        index_column = 'recipe_id'
+
+        pd_df_all = create_parameters_table(similarity_type=similarity_type,
+                                            columns=columns_all,
+                                            index_column=index_column)
+
+        self.assertEqual(pd_df_all.shape[0], 1)
+        self.assertEqual(pd_df_all.shape[1], 3)
+
+        pd_df_sub = create_parameters_table(similarity_type=similarity_type,
+                                            columns=columns_sub,
+                                            index_column=index_column)
+
+        self.assertEqual(pd_df_sub.shape[0], 1)
+        self.assertEqual(pd_df_sub.shape[1], 3)
+
+        columns_sub_check = list(pd_df_sub['columns'].values)[0]
+        self.assertEqual(columns_sub_check, 'country, protein')
 
--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,5 @@
 import datetime
+import pandas as pd
 
 
 def create_timestamp():
@@ -10,4 +11,22 @@ def create_timestamp():
 
     timestamp = datetime.datetime.now().strftime('%Y%m%d_%H%M')
     return timestamp
+
+
+def create_parameters_table(similarity_type, columns, index_column):
+    """
+    Creates a table with run parameters.
+
+    :param similarity_type: string
+    :param columns: string or list of strings
+    :param index_column: string
+    :return: pandas data frame
+    """
+
+    if isinstance(columns, list):
+        columns = ', '.join(columns)
+    data = [similarity_type, columns, index_column]
+    pd_df = pd.DataFrame(columns=['similarity_type', 'columns', 'index_column'], data=[data])
+    return pd_df
+
 
